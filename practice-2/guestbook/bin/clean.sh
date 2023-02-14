185a53a958688955bcde787e75d268a5e8fa3638
Julien <julien.quennehen@edu.devinci.fr>
2023-02-14 16:51:54 +0100

    .sh file

---
+++ practice-2/guestbook/bin/clean.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+kubectl delete deployment -l app=redis
+kubectl delete service -l app=redis
+kubectl delete deployment frontend
+kubectl delete service frontend#Si on utilise 
+
+echo "everything deleted"
